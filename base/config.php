0dddda3f18e443d1743f261e548cf9af7a6a3cab
Ekaterina Tagieva <ek.tagieva@gmail.com>
2024-02-12 15:33:43 +0300

    project4 init

---
+++ base/config.php
@@ -0,0 +1,15 @@
+<?php
+const DB_USER = 'stage';
+const DB_NAME = 'stage';
+//const DB_HOST = 'localhost:33060';
+const DB_HOST = 'mysql_db:3306';
+const DB_PASSWORD = 'stage';
+const HOST_SITE = 'http://project4.loc';
+
+const ADMIN_IDS = [9];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
---
+++ html/index.php
@@ -0,0 +1,14 @@
+<?php
+require '../vendor/autoload.php';
+require '../base/config.php';
+ini_set('display_errors', 'on');
+ini_set('error_reporting', E_ALL | E_NOTICE);
+
+$route = new \Base\Route();
+$route->add('/', \App\Controller\LoginController::class);
+$route->add('/login/register', \App\Controller\LoginController::class, 'register');
+$route->add('/login/auth', \App\Controller\LoginController::class, 'auth');
+$route->add('/blog', \App\Controller\BlogController::class);
+$route->add('/blog/addMessage', \App\Controller\BlogController::class, 'addMessage');
+$app = new \Base\Application($route);
+$app->run();
